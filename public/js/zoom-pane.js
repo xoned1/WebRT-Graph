dbc40522750a0078228bf0eb3b42c8ceeee01942
Timo Lange <itimo.lange@googlemail.com>
2019-06-30 22:36:43 +0200

    Partly implemented "fit" view function

--- public/js/zoom-pane.js
+++ public/js/zoom-pane.js
@@ -40,6 +40,40 @@ function endDragNode(d) {
 }
 
 function zoomed() {
+    // console.log(d3.event.transform)
+    //d3.select("svg").translate(d3.event.transform.x, d3.event.transform.y);
     svg.attr("transform", d3.event.transform);
 }
 
+function zoomFit(paddingPercent, transitionDuration) {
+    var root = svg;
+    var bounds = root.node().getBBox();
+    var parent = root.node().parentElement;
+    var fullWidth = parent.clientWidth,
+        fullHeight = parent.clientHeight;
+    var width = bounds.width,
+        height = bounds.height;
+    var midX = bounds.x + width / 2,
+        midY = bounds.y + height / 2;
+    if (width == 0 || height == 0) return; // nothing to fit
+    var scale = (paddingPercent || 0.75) / Math.max(width / fullWidth, height / fullHeight);
+    var translate = [fullWidth / 2 - scale * midX, fullHeight / 2 - scale * midY];
+
+    console.log("zoomFit", translate, scale);
+
+    var zoom = d3.zoom();
+
+    var transform = d3.zoomTransform(root.node());
+    transform = transform.translate(100, 0); //TODO
+    transform = transform.scale(scale);
+    svg.attr("transform", transform);
+
+    // root
+    // 	.transition()
+    // 	.duration(transitionDuration || 0) // milliseconds
+    // 	.call(zoom.translate(translate).scale(scale).event);
+}
+
+function bla() {
+    console.log(bla)
+}
